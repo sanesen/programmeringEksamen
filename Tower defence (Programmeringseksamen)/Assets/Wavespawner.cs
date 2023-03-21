d74286405a53d7d52eca785ca5bdf543430977bf
Kristine Hørlück <kristine.horluck@outlook.com>
2023-03-21 09:35:18 +0100

    Push

--- Tower defence (Programmeringseksamen)/Assets/Wavespawner.cs
+++ Tower defence (Programmeringseksamen)/Assets/Wavespawner.cs
@@ -2,11 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wave
-{
-    public string waveName;
-}
+
 public class Wavespawner : MonoBehaviour
 {
+    private const float V = 2f;
+    public Transform enemyPrefab;
 
+    public float CountdownTime = 10f;
+    private float countdown = V;
+
+    void Update()
+    {
+        if (countdown <=0f)
+        {
+            SpawnWave();
+            countdown = CountdownTime;
+        }
+        countdown -= Time.deltaTime;
+        
+    }
+    void SpawnWave()
+    {
+        Debug.Log("Wave Incomming");
+    }
 }
