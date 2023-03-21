7e6a11a50e5358583985677bc66dad82e1b593c0
Kristine Hørlück <kristine.horluck@outlook.com>
2023-03-21 08:06:58 +0100

    Opsætning Fjender

---
+++ Tower defence (Programmeringseksamen)/Assets/Wavespawner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave
+{
+    public string waveName;
+}
+public class Wavespawner : MonoBehaviour
+{
+
+}
