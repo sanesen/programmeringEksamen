09a1494d7c476e8dfa2e9752783a6ad711aa75da
sanesen-Laptop <sanesen2003@gmail.com>
2023-03-23 11:39:12 +0100

    Revert "Merge branch 'main' of https://github.com/sanesen/programmeringEksamen"
    This reverts commit 6c12631dc67350e01d85bc350f087750de98eada, reversing
    changes made to 53f368416dd6f97ec29d8c1bcdb6fc8ee06263d1.

--- Tower defence (Programmeringseksamen)/Assets/Scripts/Kristine/Movement.cs
+++ Tower defence (Programmeringseksamen)/Assets/Scripts/Kristine/Movement.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public static Transform[] points;
-    private void Awake()
+    private void Start ()
     {
-
         points = new Transform[transform.childCount];
         for (int i = 0; i < points.Length; i++)
         {
-            points[i] = transform.GetChild(i);
+            points[i]=transform.GetChild(i);
+
         }
+        print(points.Length);
     }
 }
