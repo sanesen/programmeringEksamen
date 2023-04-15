cafa708192d10b5caf33c3e8db0e0c32f80bf37b
Kristine Hørlück <kristine.horluck@outlook.com>
2023-04-15 21:34:01 +0200

    Pseudokode

--- Tower defence (Programmeringseksamen)/Assets/Scripts/Kristine/EnemyMovement.cs
+++ Tower defence (Programmeringseksamen)/Assets/Scripts/Kristine/EnemyMovement.cs
@@ -13,28 +13,32 @@ public class EnemyMovement : MonoBehaviour
     {
         rute = ruteManager.Instance.transform;
 
-
+        //Indsætter hvor mange punkter der er på ruten
         Transform[] _rute = new Transform[16];
 
-
+        //Kasper Schnejder B20
         for (int i = 0; i < _rute.Length; i++)
         {
             _rute[i] = rute.GetChild(i);
 
         }
+        
         StartCoroutine(GoThroughRoute(_rute));
     }
+  
     IEnumerator GoThroughRoute(Transform[] rute)
     {
-        Vector2 origin = Vector2.zero;
-        Vector2 target = Vector2.zero;
+        Vector2 origin = Vector2.zero; //sætter Origin til 0
+        Vector2 target = Vector2.zero; //Sætter mål til 0
 
-        for (int i = 0; i < rute.Length - 1; i++)
+        //Forloop, som tilføjer hvert point, som det næste mål i rækkefølgen
+        for (int i = 0; i < rute.Length - 1; i++) // i er en variable
         {
-            origin = rute[i].position;
-            target = rute[i + 1].position;
+            origin = rute[i].position; // Hvergang dette kode kører, så ændre vores origin punkt fra 0 til den tidl. defineret rute[i] som er et punkt på ruten
+            target = rute[i + 1].position; // Det næste mål på ruten
 
-            if (target.x - origin.x > 0.1 && target.y - origin.y < 0.1 && target.y - origin.y > -0.1)
+            //Bestemmer hvilken retning fjenden skal vende
+            if (target.x - origin.x > 0.1 && target.y - origin.y < 0.1 && target.y - origin.y > -0.1) 
             {
                 transform.rotation = Quaternion.Euler(0, 0, 0);
             }
@@ -51,6 +55,7 @@ public class EnemyMovement : MonoBehaviour
                 transform.rotation = Quaternion.Euler(0, 0, 180);
             }
 
+            //Bestemmer farten for fjenden mellem punkterne
             float timeToGo = Vector2.Distance(origin, target) / speed;
             float timeElasped = 0;
 
